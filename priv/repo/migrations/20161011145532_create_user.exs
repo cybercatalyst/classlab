1a994386efa690e09234b987cb0a89074f19be6e
Sascha Brink <sascha.brink@gmail.com>
2016-10-13 16:45:02 +0200

    Update user migration fields

--- priv/repo/migrations/20161011145532_create_user.exs
+++ priv/repo/migrations/20161011145532_create_user.exs
@@ -3,12 +3,12 @@ defmodule Classlab.Repo.Migrations.CreateUser do
 
   def change do
     create table(:users) do
+      add :email, :string, null: false
       add :first_name, :string
       add :last_name, :string
-      add :email, :string, null: false
+      add :superadmin, :boolean, default: false, null: false
       add :access_token, null: false, :string
       add :access_token_expired_at, null: false, :datetime
-      add :superadmin, :boolean, default: false, null: false
       timestamps()
     end
 
