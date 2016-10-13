b66d0cd47e1fadfcf004ff48dee242b30d796a5f
Sascha Brink <sascha.brink@gmail.com>
2016-10-13 17:16:42 +0200

    Add location model required restrictions

--- priv/repo/migrations/20161012151240_create_location.exs
+++ priv/repo/migrations/20161012151240_create_location.exs
@@ -3,12 +3,12 @@ defmodule Classlab.Repo.Migrations.CreateLocation do
 
   def change do
     create table(:locations) do
-      add :name, :string
-      add :address_line_1, :string
+      add :name, :string, null: false
+      add :address_line_1, :string, null: false
       add :address_line_2, :string
-      add :zipcode, :string
-      add :city, :string
-      add :country, :string
+      add :zipcode, :string, null: false
+      add :city, :string, null: false
+      add :country, :string, null: false
       add :external_url, :string
       timestamps()
     end
