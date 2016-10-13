f94b71e365939d90e4905ca47b5b399eed2f37ea
Sascha Brink <sascha.brink@gmail.com>
2016-10-13 21:52:08 +0200

    Add router path to local mails

--- web/router.ex
+++ web/router.ex
@@ -1,6 +1,10 @@
 defmodule Classlab.Router do
   use Classlab.Web, :router
 
+  if Mix.env == :dev do
+    forward "/sent_emails", Bamboo.EmailPreviewPlug
+  end
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
