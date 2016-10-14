b1928005d50c326c46b2986fbb06a9e2fdbc000e
Martin Schurig <martin@schurig.pw>
2016-10-14 15:41:53 +0200

    test assign_user_plug

--- test/lib/plugs/assign_user_plug_test.exs
+++ test/lib/plugs/assign_user_plug_test.exs
@@ -1,9 +1,19 @@
 defmodule Classlab.AssignUserPlugTest do
-  alias Classlab.AssignUserPlug
+  alias Classlab.{AssignUserPlug, Session}
 
   use Classlab.ConnCase
 
-  test "#", %{conn: conn} do
+  test "VALID user_id_jwt in session it assigns the correct user", %{conn: conn} do
+    user = Factory.insert(:user)
+    conn =
+      conn
+      |> Session.login(user)
+      |> AssignUserPlug.call(%{})
+
+    assert conn.assigns[:current_user] == user
+  end
+
+  test "INVALID user_id_jwt in session it assigns nil for the user", %{conn: conn} do
     conn = AssignUserPlug.call(conn, %{})
     refute conn.assigns[:current_user]
   end
