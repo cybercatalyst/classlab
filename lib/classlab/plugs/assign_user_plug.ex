17286f3583bbbc210e4a8888da6367348cd289fd
Bengt Weiße <bengtler@gmail.com>
2016-10-14 13:56:57 +0200

    redirect to session on if invalid jwt token

--- lib/classlab/plugs/assign_user_plug.ex
+++ lib/classlab/plugs/assign_user_plug.ex
@@ -1,6 +1,7 @@
 defmodule Classlab.AssignUserPlug do
   @moduledoc false
-  alias Classlab.{JWT.UserIdToken, Repo, User}
+  use Phoenix.Controller
+  alias Classlab.{JWT.UserIdToken, Repo, Router.Helpers, User}
   import Plug.Conn
 
   def init(opts), do: opts
@@ -8,15 +9,24 @@ defmodule Classlab.AssignUserPlug do
   def call(conn, _opts) do
     user_id_jwt = get_session(conn, :user_id_jwt)
 
+    decode_jwt_token(conn, user_id_jwt)
+  end
+
+  defp decode_jwt_token(conn, nil) do assign_current_user(conn) end
+  defp decode_jwt_token(conn, user_id_jwt) do
     case UserIdToken.decode(user_id_jwt) do
       %{"user_id" => user_id} ->
         assign_current_user(conn, user_id)
       _ ->
-        assign_current_user(conn, nil)
+        delete_session(conn, :user_id_jwt)
+
+        conn
+        |> put_flash(:info, "Permission denied.")
+        |> redirect(to: Helpers.session_path(conn, :new))
+        |> halt
     end
   end
-
-  defp assign_current_user(conn, nil) do
+  defp assign_current_user(conn) do
     assign(conn, :current_user, nil)
   end
   defp assign_current_user(conn, user_id) do
