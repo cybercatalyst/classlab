8a808dc666d287fc921b9f0d619c96d01f4c2b2b
Sascha Brink <sascha.brink@gmail.com>
2016-10-14 14:37:57 +0200

    Add current_user helper

--- lib/classlab/plugs/assign_user_plug.ex
+++ lib/classlab/plugs/assign_user_plug.ex
@@ -1,17 +1,21 @@
 defmodule Classlab.AssignUserPlug do
   @moduledoc false
-  use Phoenix.Controller
   alias Classlab.{JWT.UserIdToken, Repo, Router.Helpers, User}
+  use Phoenix.Controller
   import Plug.Conn
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
     user_id_jwt = get_session(conn, :user_id_jwt)
-
     decode_jwt_token(conn, user_id_jwt)
   end
 
+  def current_user(conn) do
+    conn.assigns[:current_user]
+  end
+
+  # Private methods
   defp decode_jwt_token(conn, nil) do assign_current_user(conn) end
   defp decode_jwt_token(conn, user_id_jwt) do
     case UserIdToken.decode(user_id_jwt) do
