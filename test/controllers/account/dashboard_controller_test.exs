db37536bcebeaadc48b51d975cff51e48866fb07
Sascha Brink <sascha.brink@gmail.com>
2016-10-17 17:37:23 +0200

    Add option to edit user profile

--- test/controllers/account/dashboard_controller_test.exs
+++ test/controllers/account/dashboard_controller_test.exs
@@ -8,8 +8,17 @@ defmodule Classlab.Account.DashboardControllerTest do
 
   describe "#show" do
     test "shows chosen resource", %{conn: conn} do
+      event_as_owner =
+        Factory.insert(:event,
+        memberships: [Factory.insert(:membership, user: current_user(conn), role_id: 1)])
+      event_as_attendee =
+        Factory.insert(:event,
+          memberships: [Factory.insert(:membership, user: current_user(conn), role_id: 3)])
+
       conn = get conn, account_dashboard_path(conn, :show)
       assert html_response(conn, 200) =~ "Dashboard"
+      assert html_response(conn, 200) =~ event_as_owner.name
+      assert html_response(conn, 200) =~ event_as_attendee.name
     end
   end
 end
--- web/controllers/account/dashboard_controller.ex
+++ web/controllers/account/dashboard_controller.ex
@@ -1,10 +1,21 @@
 defmodule Classlab.Account.DashboardController do
+  alias Classlab.Event
   use Classlab.Web, :controller
 
   def show(conn, _params) do
     user =
       current_user(conn)
 
-    render(conn, "show.html", user: user)
+    events_as_owner =
+      Event
+      |> Event.as_role(user, 1)
+      |> Repo.all()
+
+    events_as_attendee =
+      Event
+      |> Event.as_role(user, 3)
+      |> Repo.all()
+
+    render(conn, "show.html", user: user, events_as_owner: events_as_owner, events_as_attendee: events_as_attendee)
   end
 end
