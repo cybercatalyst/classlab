3191f5c5768ae3379cb22eff4e4f904f9e9dcf3a
Bengt Weiße <bengtler@gmail.com>
2016-10-18 16:35:34 +0200

    add missing tests for assign_user_plug

--- test/lib/plugs/assign_user_plug_test.exs
+++ test/lib/plugs/assign_user_plug_test.exs
@@ -1,20 +1,38 @@
 defmodule Classlab.AssignUserPlugTest do
-  alias Classlab.{AssignUserPlug, Session}
+  alias Classlab.{AssignUserPlug, JWT.UserIdToken, Session}
 
   use Classlab.ConnCase
 
-  test "VALID user_id_jwt in session it assigns the correct user", %{conn: conn} do
-    user = Factory.insert(:user)
-    conn =
-      conn
-      |> Session.login(user)
-      |> AssignUserPlug.call(%{})
+  describe "#call" do
+    test "VALID user_id_jwt in session it assigns the correct user", %{conn: conn} do
+      user = Factory.insert(:user)
+      conn =
+        conn
+        |> Session.login(user)
+        |> AssignUserPlug.call(%{})
 
-    assert conn.assigns[:current_user] == user
+      assert conn.assigns[:current_user] == user
+    end
+
+    test "INVALID user_id_jwt in session it assigns nil for the user", %{conn: conn} do
+      conn = AssignUserPlug.call(conn, %{})
+      refute conn.assigns[:current_user]
+    end
+
+    test "user_id_jwt in session calls login", %{conn: conn} do
+      user = Factory.insert(:user)
+
+      conn =
+        conn
+        |> put_session(:user_id_jwt, UserIdToken.encode(%UserIdToken{user_id: user.id}))
+        |> AssignUserPlug.call(%{})
+
+      assert conn.assigns[:current_user] == user
+    end
   end
 
-  test "INVALID user_id_jwt in session it assigns nil for the user", %{conn: conn} do
-    conn = AssignUserPlug.call(conn, %{})
-    refute conn.assigns[:current_user]
+  describe "#init" do
+    opts = %{"test" => true}
+    assert AssignUserPlug.init(opts) == opts
   end
 end
