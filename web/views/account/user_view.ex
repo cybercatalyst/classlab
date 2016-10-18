5e13da45d1bcf95eb7dc6722f659a021fe07ab7c
Sascha Brink <sascha.brink@gmail.com>
2016-10-18 15:34:30 +0200

    Remove unused page helper

--- web/views/account/user_view.ex
+++ web/views/account/user_view.ex
@@ -2,9 +2,6 @@ defmodule Classlab.Account.UserView do
   use Classlab.Web, :view
 
   # Page Configuration
-  def page("show.html", _conn), do: %{
-    title: "User profile"
-  }
   def page("edit.html", _conn), do: %{
     title: "Edit User profile"
   }
