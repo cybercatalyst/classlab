ee6f48359368967a7535f344427e1343b3de7c5b
Sascha Brink <sascha.brink@gmail.com>
2016-10-18 17:01:35 +0200

    Improve user token tests

--- test/lib/jwt/user_token_test.exs
+++ test/lib/jwt/user_token_test.exs
@@ -11,8 +11,17 @@ defmodule Classlab.JWT.UserTokenTest do
 
   describe "#decode" do
     @valid_token "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxfQ.Fl_dAGOvt8rPcvFlqoAI7z5_WUqGU9ATv9ySKi029kI"
-    test "return a map from a VALID jwt" do
+    test "returns a UserToken struct from a VALID jwt" do
       assert UserToken.decode(@valid_token) == %UserToken{user_id: 1}
+    end
+
+    @invalid_token "invalid_token"
+    test "returns nil from an INVALID jwt" do
+      assert UserToken.decode(@invalid_token) == nil
+    end
+
+    test "returns nil if no token at all" do
+      assert UserToken.decode(nil) == nil
     end
   end
 end
