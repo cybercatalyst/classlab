277b59d8f3f3bb52a24f67fa11e86550d467845a
Sascha Brink <sascha.brink@gmail.com>
2016-10-18 15:55:13 +0200

    Remove unused calendar helpers

--- web/views/calendar_helpers.ex
+++ web/views/calendar_helpers.ex
@@ -4,16 +4,6 @@ defmodule Classlab.CalendarHelpers do
   """
   alias Calendar.Strftime
 
-  def l(%Date{} = date, [format: format]) do
-    Strftime.strftime!(date, format, :de)
-  end
-
-  def l(%DateTime{} = date, [format: format]) do
-    date
-    # |> Calendar.DateTime.shift_zone!("Europe/Berlin")
-    |> Strftime.strftime!(format, :de)
-  end
-
   def l(%Date{} = date) do
     Strftime.strftime!(date, "%d.%m.%Y")
   end
