2a344cc42bdc2515f163c269c02c18ffb616388b
Bengt Weiße <bengtler@gmail.com>
2016-10-19 13:32:24 +0200

    close #3: create test superadmin in seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,7 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-alias Classlab.{Repo, Role}
+alias Calendar
+alias Classlab.{Repo, Role, User}
+
+Repo.insert!(%User{
+  id: 1,
+  first_name: "Super",
+  last_name: "Admin",
+  email: "info@classlab.org",
+  access_token: "1234",
+  access_token_expired_at: Calendar.DateTime.now_utc(),
+  superadmin: true
+})
 
 Repo.insert!(%Role{id: 1, name: "Owner"})
 Repo.insert!(%Role{id: 2, name: "Trainer"})
