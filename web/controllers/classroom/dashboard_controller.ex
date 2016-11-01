1f7e69dde7b233ea7bd4dadc06253ebd184e4e88
Bengt Weiße <bengtler@gmail.com>
2016-11-01 11:36:45 +0100

    style classroom dashboard

--- web/controllers/classroom/dashboard_controller.ex
+++ web/controllers/classroom/dashboard_controller.ex
@@ -36,6 +36,7 @@ defmodule Classlab.Classroom.DashboardController do
       |> Query.order_by(desc: :inserted_at)
       |> Query.limit(5)
       |> Repo.all()
+      |> Repo.preload(:user)
 
     latest_memberships =
       event
