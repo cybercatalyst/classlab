72fe52d97ee02b95593ce8d570365e55fa2b1070
Bengt Weiße <bengtler@gmail.com>
2016-11-01 10:49:20 +0100

    fill classroom dashboard with data

--- web/controllers/classroom/dashboard_controller.ex
+++ web/controllers/classroom/dashboard_controller.ex
@@ -1,6 +1,7 @@
 defmodule Classlab.Classroom.DashboardController do
   @moduledoc false
-  alias Classlab.Feedback
+  alias Classlab.{Feedback, Material, Membership, Task}
+  alias Ecto.Query
   use Classlab.Web, :controller
 
   def show(conn, _params) do
@@ -12,7 +13,49 @@ defmodule Classlab.Classroom.DashboardController do
       |> Feedback.averages()
       |> Repo.one()
 
-    render(conn, "show.html", event: event, feedback_averages: feedback_averages)
+    latest_unlocked_tasks =
+      event
+      |> assoc(:tasks)
+      |> Task.unlocked()
+      |> Query.order_by(desc: :inserted_at)
+      |> Query.limit(5)
+      |> Repo.all()
+
+    latest_unlocked_materials =
+      event
+      |> assoc(:materials)
+      |> Material.unlocked()
+      |> Query.order_by(desc: :inserted_at)
+      |> Query.order_by(desc: :inserted_at)
+      |> Query.limit(5)
+      |> Repo.all()
+
+    latest_chat_messages =
+      event
+      |> assoc(:chat_messages)
+      |> Query.order_by(desc: :inserted_at)
+      |> Query.limit(5)
+      |> Repo.all()
+
+    latest_memberships =
+      event
+      |> assoc(:memberships)
+      |> Membership.not_as_role(1)
+      |> Query.order_by(desc: :inserted_at)
+      |> Query.limit(5)
+      |> Repo.all()
+      |> Repo.preload(:user)
+
+    render(
+      conn,
+      "show.html",
+      event: event,
+      feedback_averages: feedback_averages,
+      latest_unlocked_tasks: latest_unlocked_tasks,
+      latest_unlocked_materials: latest_unlocked_materials,
+      latest_chat_messages: latest_chat_messages,
+      latest_memberships: latest_memberships
+    )
   end
 
   # Private methods
