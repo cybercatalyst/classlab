1249a8cf7ab19727639032debfd0bfa3d7de8782
Sascha Brink <sascha.brink@gmail.com>
2016-11-03 15:52:14 +0100

    Disable origin check in prod (temporary)

--- config/prod.exs
+++ config/prod.exs
@@ -18,6 +18,7 @@ config :classlab, Classlab.Endpoint,
         port: get_env("CLASSLAB_URL_PORT") || get_env("PORT") || 80],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: get_env("SECRET_KEY_BASE"),
+  check_origin: false,
   server: true
 
 # Do not print debug messages in production
